43837c1511bc9506cfef8e303363655c3562a293
Naveen G <naveeng2402@gmail.com>
2022-07-30 22:17:36 +0530

    Added Markers

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,76 @@
-import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import {
+  Map,
+  Marker,
+  Layer,
+  Source,
+  GeolocateControl,
+  NavigationControl,
+  MapRef,
+  GeolocateControlRef,
+  LngLat,
+} from "react-map-gl";
+
+import data from "./assets/data.json";
+import { MarkerShape } from "./types/markers";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const mapRef = useRef<MapRef>(null);
+  const geoLocateControlRef = useRef<GeolocateControlRef>(null);
+
+  const [markers, setMarkers] = useState<MarkerShape[]>([]);
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+  const [selectedData, setSelectedData] = useState<MarkerShape>();
+  const [startLocation, setStartLocation] = useState<LngLat>();
+  const [destLocation, setDestLocation] = useState<LngLat>();
+  const [route, setRoute] = useState<number[][]>([]);
+
+  useEffect(() => {
+    setMarkers(data);
+
+    // TODO: Fetch the data from api
+  }, []);
+
+  const initialViewState = {
+    latitude: 13.02073768459028,
+    longitude: 80.21538956597244,
+    zoom: 18,
+  };
 
   return (
-    <div className="App">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="h-screen">
+      <Map
+        ref={mapRef}
+        initialViewState={initialViewState}
+        mapStyle="mapbox://styles/naveeng2402/cl65xxp8n000915mgico9fy0u"
+        mapboxAccessToken={import.meta.env.VITE_API_KEY}
+        attributionControl={false}
+        onLoad={() => {
+          geoLocateControlRef.current?.trigger();
+        }}
+      >
+        <NavigationControl position="bottom-right" visualizePitch={true} />
+        <GeolocateControl
+          ref={geoLocateControlRef}
+          showUserHeading={true}
+          showAccuracyCircle={true}
+          position="bottom-right"
+        />
+
+        {markers.map((marker, index) => (
+          <Marker
+            key={index}
+            latitude={marker.lat}
+            longitude={marker.lon}
+            onClick={(e) => {
+              // console.log(marker);
+              setIsSelected(true);
+              setSelectedData(marker);
+            }}
+          ></Marker>
+        ))}
+      </Map>
     </div>
   );
 }
---
+++ src/types/markers.d.ts
@@ -0,0 +1,5 @@
+export interface MarkerShape {
+  place: string;
+  lat: number;
+  lon: number;
+}
