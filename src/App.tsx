fb222b2a4160af2d3af1c6c13f56bb396ec2f6e0
Naveen G <naveeng2402@gmail.com>
2022-07-31 12:44:59 +0530

    Fixed exitNavigation position

--- src/App.tsx
+++ src/App.tsx
@@ -96,91 +96,94 @@ function App() {
             ></Marker>
           ))}
 
-        <AnimatePresence>
-          {route.length > 0 && (
-            <>
-              <Source
+        {route.length > 0 && (
+          <>
+            <Source
+              id="start"
+              type="geojson"
+              data={getLocationGeojson(startLocation)}
+            >
+              <Layer
                 id="start"
-                type="geojson"
-                data={getLocationGeojson(startLocation)}
-              >
-                <Layer
-                  id="start"
-                  type="symbol"
-                  layout={{
-                    "icon-image": "dot-10",
-                    "icon-size": 1,
-                  }}
-                ></Layer>
-              </Source>
-              <Source
+                type="symbol"
+                layout={{
+                  "icon-image": "dot-10",
+                  "icon-size": 1,
+                }}
+              ></Layer>
+            </Source>
+            <Source
+              id="dest"
+              type="geojson"
+              data={getLocationGeojson(destLocation)}
+            >
+              <Layer
                 id="dest"
-                type="geojson"
-                data={getLocationGeojson(destLocation)}
-              >
-                <Layer
-                  id="dest"
-                  type="symbol"
-                  layout={{
-                    "icon-image": "charging-station",
-                    "icon-size": 2,
-                    "icon-allow-overlap": true,
-                  }}
-                ></Layer>
-              </Source>
-              <Source
+                type="symbol"
+                layout={{
+                  "icon-image": "charging-station",
+                  "icon-size": 2,
+                  "icon-allow-overlap": true,
+                }}
+              ></Layer>
+            </Source>
+            <Source
+              id="route"
+              type="geojson"
+              data={getRouteData(route as number[][])}
+            >
+              <Layer
                 id="route"
-                type="geojson"
-                data={getRouteData(route as number[][])}
-              >
-                <Layer
-                  id="route"
-                  type="line"
-                  layout={{
-                    "line-join": "round",
-                    "line-cap": "round",
-                  }}
-                  paint={{
-                    "line-color": "#006b00",
-                    "line-width": 5,
-                    "line-opacity": 0.5,
-                  }}
-                ></Layer>
-              </Source>
-
-              <motion.div
-                variants={exitNavigationAnim}
-                initial="hidden"
-                exit="hidden"
-                animate="visible"
-                className="absolute bottom-0 left-0 right-0 mb-4 flex items-center justify-center"
-              >
-                <button
-                  className="rounded-md bg-red-500 px-10 py-2 text-xl text-white transition-all duration-100 hover:bg-red-600 "
-                  onClick={() => {
-                    setDestLocation([]);
-                    setRoute([]);
-                    setDestDesc(undefined);
-                  }}
-                >
-                  Exit Navigation
-                </button>
-              </motion.div>
-
-              <motion.div
-                variants={destDescAnim}
-                initial="hidden"
-                exit="hidden"
-                animate="visible"
-                className="absolute top-0 left-0 mx-4 my-2 rounded-sm border-2 border-gray-800 bg-gray-600/80 px-4 py-2 text-white"
-              >
-                <p className="text-base">{destDesc?.place}</p>
-                <p className="text-base">{destDesc?.distance}</p>
-              </motion.div>
-            </>
-          )}
-        </AnimatePresence>
+                type="line"
+                layout={{
+                  "line-join": "round",
+                  "line-cap": "round",
+                }}
+                paint={{
+                  "line-color": "#006b00",
+                  "line-width": 5,
+                  "line-opacity": 0.5,
+                }}
+              ></Layer>
+            </Source>
+          </>
+        )}
       </Map>
+      <AnimatePresence>
+        {route.length > 0 && (
+          <>
+            <motion.div
+              variants={exitNavigationAnim}
+              initial="hidden"
+              exit="hidden"
+              animate="visible"
+              className="absolute bottom-0 left-0 right-0 mb-4 flex items-center justify-center"
+            >
+              <button
+                className="rounded-md bg-red-500 px-10 py-2 text-xl text-white transition-all duration-100 hover:bg-red-600 "
+                onClick={() => {
+                  setDestLocation([]);
+                  setRoute([]);
+                  setDestDesc(undefined);
+                }}
+              >
+                Exit Navigation
+              </button>
+            </motion.div>
+
+            <motion.div
+              variants={destDescAnim}
+              initial="hidden"
+              exit="hidden"
+              animate="visible"
+              className="absolute top-0 left-0 mx-4 my-2 rounded-sm border-2 border-gray-800 bg-gray-600/80 px-4 py-2 text-white"
+            >
+              <p className="text-base">{destDesc?.place}</p>
+              <p className="text-base">{destDesc?.distance}</p>
+            </motion.div>
+          </>
+        )}
+      </AnimatePresence>
 
       <AnimatePresence>
         {isSelected && (
